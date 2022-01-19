f51ff89a4b73baea6f3939bed74c4830a17bed77
spencer barriball <sbarriball@me.com>
2022-01-19 19:35:02 +0000

    add filesystem commands using fs/promises library

--- index.mjs
+++ index.mjs
@@ -1,3 +1,15 @@
-export const action = () => {
-  console.log('hello');
+import { readFile, writeFile } from 'fs/promises'
+
+let template = await readFile(new URL('./template.html', import.meta.url), 'utf-8')
+
+const data = {
+  title: 'My new file',
+  body: 'I wrote this file to disk using node'
 }
+
+for (const [key, val] of Object.entries(data)) {
+  template = template.replace(`{${key}}`, val)
+}
+
+console.log(template);
+await writeFile(new URL('./index.html', import.meta.url), template)
