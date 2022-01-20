2ca82fa1ed9998102b24447088647766ae5d7c74
spencer barriball <sbarriball@me.com>
2022-01-20 18:55:21 +0000

    hotfix spelling error

--- package/reddit.mjs
+++ package/reddit.mjs
@@ -10,7 +10,7 @@ const { argv } = yargs(process.argv)
 const res = await fetch('https://www.reddit.com/.json')
 const data = await res.json()
 const randomIndex = Math.floor(Math.random() * data.data.children.length)
-// get radom post from reddit api response of all posts on front page
+// get random post from reddit api response of all posts on front page
 const post = data.data.children[randomIndex]
 
 // log if --print flag is passed
